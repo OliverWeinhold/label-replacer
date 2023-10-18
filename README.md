b2e73d3819cbdcfdab1f42ca7a78b200accbed25
Oliver Weinhold <71061042+OliverWeinhold@users.noreply.github.com>
2023-10-18 21:02:08 +0200

    Initial commit
    Added Label Replacer PHP Code, Readme and License

---
+++ README.md
@@ -0,0 +1,56 @@
+# Label Replacer WordPress Plugin
+
+The Label Replacer is a simple WordPress plugin designed to automatically replace specific text labels on your website. With an easy-to-use syntax, you can define labels and have them replaced within your website's text with your desired text. This plugin is perfect for changing terms or phrases across your pages without having to edit each page individually.
+
+## Features
+- Define unique labels easily with a simple syntax.
+- Automatically replace labels within your text.
+- Define labels anywhere on the page
+- Also works with embedded content and plugins (e.g. Insert Pages Plugin)
+
+## Installation
+
+1. Download the plugin files from this repository.
+2. Navigate to your WordPress admin, go to `Plugins -> Add New -> Upload Plugin`.
+3. Choose the plugin file you downloaded and click `Install Now`.
+4. After installation, click `Activate` to activate the plugin.
+
+## Usage
+
+1. Define a label anywhere in your post or page content using the following syntax:
+   ```plaintext
+   {{label:YourLabel=YourReplacementText}}
+   ```
+   Example:
+   ```plaintext
+   {{label:DeviceVersion=v1.0}}
+   ```  
+2. Now, wherever you want to replace the label with the text, use the following syntax:
+   ```plaintext
+   {{label:DeviceVersion}}
+   ```  
+   Example:
+   ```plaintext
+   This Guide is for Device Version {{label:DeviceVersion}}.
+   ```   
+3. The plugin will automatically replace the label with the defined text:
+   ```plaintext
+   This Guide is for Device Version v1.0.
+   ``` 
+ 
+## License
+
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.html).
+
+## Author
+
+- [Oliver Weinhold](https://oliverweinhold.de/)
+
+## Support
+
+Please note that the maintenance of this plugin is not continuous and not guaranteed. For support, feature requests, or bug reporting, please use GitHub.
+
+
+## Contributions
+
+Contributions are welcome! Feel free to open a pull request or issue on GitHub!
---
+++ label-replacer.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Plugin Name: Label Replacer
+Plugin URI: https://oliverweinhold.de/
+Description: A simple plugin to replace specific text labels on the website automatically.
+Version: 1.0
+Author: Oliver Weinhold
+Author URI: https://oliverweinhold.de/
+License: GPL v3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/*
+Label Replacer WordPress Plugin
+Copyright (C) 2023 Oliver Weinhold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
+
+function replace_labels($content) {
+    // Regex to find label definitions.
+    $label_def_regex = '/\{\{label:(.*?)=(.*?)\}\}/';
+
+    // Find all label definitions in the content.
+    preg_match_all($label_def_regex, $content, $matches, PREG_SET_ORDER);
+
+    // Check if matches were found before proceeding.
+    if (empty($matches)) {
+        return $content;  // No changes if no matches were found.
+    }
+
+    // Create an associative array from the found label definitions.
+    $labels = array();
+    foreach ($matches as $match) {
+        // Check if the necessary data is present.
+        if (isset($match[1]) && isset($match[2])) {
+            $label = esc_html($match[1]);  // Data cleaning
+            $replacement = sanitize_text_field($match[2]);  // Data validation
+            $labels['{{label:' . preg_quote($label, '/') . '}}'] = $replacement;
+
+            // Remove the label definition from the content.
+            $content = str_replace($match[0], '', $content);
+        }
+    }
+
+    // Loop through each label and replace it in the content.
+    foreach ($labels as $label => $replacement) {
+        $content = str_replace($label, $replacement, $content);
+    }
+
+    // Return the modified content.
+    return $content;
+}
+add_filter('the_content', 'replace_labels');
